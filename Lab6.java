90b0132951dc543cbad0e8428c11e9b1fe83208b
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2021-02-17 21:32:45 -0800

    Update Lab6.java

--- Lab6.java
+++ Lab6.java
@@ -1,5 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 
 import ptolemy.plot.*;
 public class Lab6 {
@@ -8,7 +7,10 @@ public class Lab6 {
 	  public static final double Xmax = 2020; // Graph domain
 	  public static final int Npoint = 500;
 	  public static void main ( String [] args ) {
-	    Plot plotObj = new Plot () ; // Create Plot object
+	    String[][] precip = new String[32][];
+	    String[][] precip1 = new String[32][];
+	    //precipitation in [22] or [23]
+		Plot plotObj = new Plot () ; // Create Plot object
 	    plotObj.setTitle("Precipitation vs Year") ;
 	    plotObj.setXLabel("Year") ;
 	    plotObj.setYLabel ("Precipitation(mm)");
@@ -17,21 +19,32 @@ public class Lab6 {
 	    // plotObj.addPoint ( int Set , double x , double y , boolean connect );
 	    double xStep = (Xmax - Xmin ) / Npoint;
 	    // Plotting loop
-	    
-	    PlotApplication app = new PlotApplication ( plotObj ) ; // Display
-	    
 	    for (double x = Xmin; x <= Xmax; x += xStep) {
 	      double y = Math.sin( x ) * Math.sin ( x ) ;
 	      plotObj.addPoint (0 , x , y , true ) ;
 	    }
+	    PlotApplication app = new PlotApplication ( plotObj ) ; // Display
+	    
+	    for(int i = 1995; i <= 2020; i++) {
+	    	precip[i] = readFile("src/weather/" + i + ".csv");	    	
+	    }
+	    
+	    for(int i = 1995; i <= 2020; i++) {
+	    	precip1[i] = readFile("src/weather2/" + i + ".csv");
+	    }
 	  }//main
 	  
-	  public static String[] readFile(String year) {
+	  public static String[] readFile(String fileName) {
 			 
-			 BufferedReader in = new BufferedReader(new FileReader("src/weather/" + year + ".txt"));
+			 BufferedReader in = new BufferedReader(System.in);
+			try {
+				in = new BufferedReader(new FileReader(fileName));
+			} catch (FileNotFoundException e) {
+			
+			}
              String[] lines = in.lines().toArray(String[]::new);
 		        
 		        return lines;
 		}//readFile
-
-}//Lab6
+	  
+}//Lab
