aabeac13961c7d433b753dc4ed726919ef4290a6
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2021-02-18 22:48:52 -0800

    Update Lab6.java

--- Lab6.java
+++ Lab6.java
@@ -5,9 +5,10 @@ public class Lab6 {
 
 	 public static final double Xmin = 1995;
 	 public static final double Xmax = 2020; // Graph domain
-	 public static final int Npoint = 500;
+	 public static final int Npoint = 5000;
 	  public static void main ( String [] args ) {
 		ArrayList<Point> data = new ArrayList<Point>(); 
+		ArrayList<Point> data2 = new ArrayList<Point>();
 		Plot plotObj = new Plot () ; // Create Plot object
 	    plotObj.setTitle("Precipitation vs Year") ;
 	    plotObj.setXLabel("Year") ;
@@ -24,6 +25,7 @@ public class Lab6 {
 	    		}
 	    		try {
 	    		Point p = new Point(Integer.parseInt(temp2[5]), Integer.parseInt(temp2[6]), Integer.parseInt(temp2[7]),Double.parseDouble(temp2[23]));
+	    		System.out.println(p.toString());
 	    		data.add(p);
 	    		} catch(Exception e) {
 	    			continue;
@@ -41,23 +43,30 @@ public class Lab6 {
 	    		}
 	    		try {
 		    		Point p = new Point(Integer.parseInt(temp2[5]), Integer.parseInt(temp2[6]), Integer.parseInt(temp2[7]),Double.parseDouble(temp2[23]));
-		    		data.add(p);
+		    		System.out.println(p.toString());
+		    		data2.add(p);
 		    		} catch(Exception e) {
 		    			continue;
 		    		}
 	    	}
 	    }
 	    
-	    
+	    for(int i = 0; i < data.size(); i++) {
+	    	if(data.get(i).getPrecip() == 0) {
+	    		data.set(i, data2.get(i));
+	    	}
+	    	
+	    }
 	    
 	    double xStep = (Xmax - Xmin ) / Npoint;
 	    // Plotting loop
 	    for (double x = Xmin; x <= Xmax; x += xStep) {
-	      double y = Math.sin(x) * Math.sin (x) ;
-	      plotObj.addPoint (0 , x, y, true) ;
+	      for(int i = 0; i < data.size(); i++) {
+	    	  double y = data.get(i).getPrecip();
+		      plotObj.addPoint (0 , x, y, true) ;
+	      }
 	    }
-	    PlotApplication app = new PlotApplication (plotObj); // Display
-	    PlotApplication plot = new PlotApplication(plotObj); //Display 
+	    PlotApplication app = new PlotApplication (plotObj); // Display 
 	    
 	  }//main
 	  
