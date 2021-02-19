b0eee2f11d0c9ea427eed1339bc665f187ee9500
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2021-02-19 12:22:43 -0800

    Add files via upload

---
+++ Lockable.java
@@ -0,0 +1,10 @@
+/**
+ * @author Surya Narayan *
+ */
+public interface Lockable {
+	public void setKey(int key);	
+	public void lock(int key);
+	public void unlock(int key);
+	public boolean locked();
+	
+}//Lockable
