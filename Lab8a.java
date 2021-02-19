c6f97ad0c984d792687fba213f1d56a1e0524bb6
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2021-02-19 12:22:06 -0800

    Update Lab8a.java

--- Lab8a.java
+++ Lab8a.java
@@ -2,34 +2,38 @@ public class Lab8a {
 
 	public static void main(String[] args) {
 		Account acct1 = new Account ("Ted Murphy", 12345, 214.56);
-	      Account acct2 = new Account ("Anita Gomez", 54321, 20.00);
-	     
+	    Account acct2 = new Account ("Anita Gomez", 54321, 20.00);
+	    Account acct3 = new Account ("Surya Narayan", 42069); 
 
 	      System.out.println ("Murphy balance after deposit: $" + acct1.deposit (14.58));
 	      System.out.println ("Gomez balance after deposit: $" + acct2.deposit (300.00));
-
+	      System.out.println("Narayan balance after deposit: $" + acct3.deposit (666.00));
 	      System.out.println ("Gomez balance after withdrawal: $" + acct2.withdraw (100.00, 2.00));
 
 		  System.out.print("\nWithdraw $800 from Gomez account: ");
 	      acct2.withdraw (800.00, 0.0);  // exceeds balance
-
+	      acct3.withdraw(420, 1);
+	      
 	      acct1.addInterest();
 	      acct2.addInterest();
+	      acct3.addInterest();
 	     
 	      System.out.println ("\nAccount Balances:");
 	      System.out.println (acct1);
 	      System.out.println (acct2);
+	      System.out.println(acct3);
 	     
 
 	      // transfer $50 from account 1 to account 2
 		  System.out.println ("\nTransfer $50 from Murphy to Gomez:");
 	      acct1.transfer(50, acct2);
-
-	   
+	      Account.transfer(180, 2, acct1, acct2);
+	      acct2.transfer(80, acct3);
 
 	      System.out.println (acct1);
 	      System.out.println (acct2);
+	      System.out.println (acct3);
 
 	}
 
-}
+}a
