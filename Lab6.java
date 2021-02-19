f4c2d94fee2a57ea33516333b3403b3fbdd04b08
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2021-02-19 11:05:22 -0800

    Update Lab6.java

--- Lab6.java
+++ Lab6.java
@@ -1,14 +1,13 @@
 import java.io.*;
 import java.util.*;
 import ptolemy.plot.*;
-public class Lab6 {
+public class Lab6{
 
 	 public static final double Xmin = 1995;
 	 public static final double Xmax = 2020; // Graph domain
 	 public static final int Npoint = 5000;
 	  public static void main ( String [] args ) {
-		ArrayList<Point> data = new ArrayList<Point>(); 
-		ArrayList<Point> data2 = new ArrayList<Point>();
+		ArrayList<Double> data = new ArrayList<Double>(); 
 		Plot plotObj = new Plot () ; // Create Plot object
 	    plotObj.setTitle("Precipitation vs Year") ;
 	    plotObj.setXLabel("Year") ;
@@ -16,53 +15,32 @@ public class Lab6 {
 	    // plotObj.setSize (400 , 300) ;
 	    // plotObj.setXRange ( Xmin , Xmax ) ;
 	    // plotObj.addPoint ( int Set , double x , double y , boolean connect );
+	    double average = 0;
 	    for(int i = 0; i <= 25; i++) {
-	    	String[] temp = readFile("src/weather/" + (i+1995) + ".csv");    	
-	    	for(int j = 0; j < temp.length; j++) {
-	    		String[]temp2 = temp[i].split("\",\"");
-	    		if(i == 0 || temp2[23] == null) {
-	    			continue;
-	    		}
-	    		try {
-	    		Point p = new Point(Integer.parseInt(temp2[5]), Integer.parseInt(temp2[6]), Integer.parseInt(temp2[7]),Double.parseDouble(temp2[23]));
-	    		System.out.println(p.toString());
-	    		data.add(p);
-	    		} catch(Exception e) {
-	    			continue;
-	    		}
-	    		
-	    	}
-	    }
-	    
-	    for(int i = 0; i <= 25; i++) {
-	    	String[] temp = readFile("src/weather2/" + (i+1995) + ".csv");    	
-	    	for(int j = 0; j < temp.length; j++) {
-	    		String[]temp2 = temp[i].split("\",\"");
-	    		if(i == 0 || temp2[23] == null) {
-	    			continue;
-	    		}
-	    		try {
-		    		Point p = new Point(Integer.parseInt(temp2[5]), Integer.parseInt(temp2[6]), Integer.parseInt(temp2[7]),Double.parseDouble(temp2[23]));
-		    		System.out.println(p.toString());
-		    		data2.add(p);
-		    		} catch(Exception e) {
+		    	String[] temp = readFile("src/weather/" + (i+1995) + ".csv");    	
+		    	String[] temp1 = readFile("src/weather2/" + (i+1995) + ".csv");
+		    	for(int j = 0; j < temp.length; j++) {
+		    		String[]temp2 = temp[i].split("\",\"");
+		    		String[] temp3 = temp1[i].split("\",\"");
+		    		if(j == 0 || (temp2[23] == null && temp3[23] == null) || (temp2[23] == "" && temp3[23] == "")) {
 		    			continue;
-		    		}
-	    	}
-	    }
-	    
-	    for(int i = 0; i < data.size(); i++) {
-	    	if(data.get(i).getPrecip() == 0) {
-	    		data.set(i, data2.get(i));
-	    	}
-	    	
+		    		}	    			
+		    		System.out.println("r");	
+		    		average = (Double.parseDouble(temp2[23]) + Double.parseDouble(temp3[23]))/2;
+		    		data.add(average);		    		
+		    }
+	    } 	
+	    System.out.println(data.size());
+	    for(Double d : data) {
+	    	System.out.println(d);
 	    }
 	    
 	    double xStep = (Xmax - Xmin ) / Npoint;
 	    // Plotting loop
 	    for (double x = Xmin; x <= Xmax; x += xStep) {
 	      for(int i = 0; i < data.size(); i++) {
-	    	  double y = data.get(i).getPrecip();
+	    	  double y = data.get(i);
+	    	 
 		      plotObj.addPoint (0 , x, y, true) ;
 	      }
 	    }
