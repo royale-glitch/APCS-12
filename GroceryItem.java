cd6d8acf0b7c7272100b07ceecea93fee845a842
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2021-02-23 21:54:48 -0800

    Update GroceryItem.java

--- GroceryItem.java
+++ GroceryItem.java
@@ -1,20 +1,21 @@
-
 public class GroceryItem {
 	private double weight;
 	private double cost;
 	private String item;
+	private int count;
 	
 	GroceryItem(){
 		this.weight = 0;
 		this.cost = 0;
 		this.item = "";
+		this.count = 0;
 	}
 	
 	public GroceryItem(double weight, double cost, String item) {
 		this.weight = weight;
 		this.cost = cost;
 		this.item = item;
-		
+		this.count = 0;
 		
 	}//GroceryItem
 	
@@ -29,6 +30,17 @@ public class GroceryItem {
 	public String getItem() {
 		return this.item;
 	}
+
+	public int getCount() {
+		return this.count;
+	}
 	
+	public void setCount(int count) {
+		this.count = count;
+	}
 	
+	public String toString() {
+		return "Name of item: " + this.getItem() + "\nPrice of item: " + this.getCost() + "\nWeight of item: " + this.getWeight();
+	}
+		
 }//GroceryItem
