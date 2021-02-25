29091112cdb851bbc1cdf24fddff154a3e0b788c
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2021-02-25 10:46:07 -0800

    Update GroceryItem.java

--- GroceryItem.java
+++ GroceryItem.java
@@ -1,14 +1,16 @@
-public class GroceryItem {
+public class GroceryItem implements Comparable<GroceryItem>{
 	private double weight;
 	private double cost;
 	private String item;
 	private int count;
+	private double ppkg;
 	
 	GroceryItem(){
 		this.weight = 0;
 		this.cost = 0;
 		this.item = "";
 		this.count = 0;
+		this.ppkg = 1;
 	}
 	
 	public GroceryItem(double weight, double cost, String item) {
@@ -16,7 +18,7 @@ public class GroceryItem {
 		this.cost = cost;
 		this.item = item;
 		this.count = 0;
-		
+		this.ppkg = this.cost / this.weight;
 	}//GroceryItem
 	
 	public double getWeight() {
@@ -35,12 +37,25 @@ public class GroceryItem {
 		return this.count;
 	}
 	
+	public double getPpKg() {
+		return this.ppkg;
+	}
+	
 	public void setCount(int count) {
 		this.count = count;
 	}
 	
 	public String toString() {
 		return "Name of item: " + this.getItem() + " Price of item: " + this.getCost() + " Weight of item: " + this.getWeight();
+	}
+
+	@Override
+	public int compareTo(GroceryItem g) {
+		if(g.ppkg > this.ppkg) {
+			return 1;
+		} else if(this.ppkg == g.ppkg) {
+			return 0;
+		} else {return -1;}
 	}
 		
 }//GroceryItem
