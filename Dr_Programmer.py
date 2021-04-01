32c71456fc75f38e9de3c32027fede1e5b34d7d6
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2021-03-31 19:59:22 -0700

    Update Dr_Programmer.py

--- Dr_Programmer.py
+++ Dr_Programmer.py
@@ -1,2 +1,29 @@
-Input = input()
-x = Input.split()
+def all_substring(binary):
+    res = [binary[i: j] for i in range(len(binary))
+          for j in range(i + 1, len(binary) + 1)]
+    candidates = []
+    for i in res:
+        zero_count = 0
+        one_count = 0
+        for j in i:
+            if j == "0":
+                zero_count += 1
+            else:
+                one_count += 1
+        if zero_count > one_count:
+            candidates.append(i)
+    return candidates            
+                
+def find_big(list):
+    big = ""
+    for i in range(len(list)):
+        if(len(big) <= len(list[i])):
+            big = list[i]
+    return len(big)
+    
+N = int(input())
+binary = str(input())
+k = all_substring(binary)
+print(find_big(k))    
+    
+    
